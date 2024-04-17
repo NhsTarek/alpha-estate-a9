3e107a2e79bc3024e09bb8c3e4b69b51ffabeb81
NhsTarek <htarek275@gmail.com>
2024-04-17 19:38:21 +0600

    loading spinner added

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -5,7 +5,12 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoutes = ({children}) => {
 
-    const {user} = useContext(AuthContext);
+    const {user, loading} = useContext(AuthContext);
+     
+    if(loading){
+        return <span className="loading loading-spinner loading-lg"></span>
+    }
+
 
     if(user){
         return children;
